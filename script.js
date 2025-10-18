75dea626874d63d5887189cb8004e1543e897a04
BigLeo147 <leozinhoscalone@gmail.com>
2025-10-18 20:55:05 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function mudarCor() {
+  const cores = ["#f2f2f2", "#ffb3b3", "#cce5ff", "#d4edda", "#fff3cd", "#f8d7da"];
+  const corAtual = document.body.style.backgroundColor;
+  let novaCor;
+
+  do {
+    novaCor = cores[Math.floor(Math.random() * cores.length)];
+  } while (novaCor === corAtual);
+
+  document.body.style.backgroundColor = novaCor;
+}
+
 
